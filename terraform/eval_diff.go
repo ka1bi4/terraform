2fe9f687f461ddc27df59835b46abe7c1c310155
Pam Selle <204372+pselle@users.noreply.github.com>
2020-09-24 12:41:50 -0400

    Update ordering for marking/unmarking and asserting plan valid
    Update when we unmark objects so we can assert the plan is valid,
    and process UnknownAsNull on the unmarked value

--- terraform/eval_diff.go
+++ terraform/eval_diff.go
@@ -280,11 +280,6 @@ func (n *EvalDiff) Eval(ctx EvalContext) (interface{}, error) {
 		panic(fmt.Sprintf("PlanResourceChange of %s produced nil value", absAddr.String()))
 	}
 
-	// Add the marks back to the planned new value
-	if len(unmarkedPaths) > 0 {
-		plannedNewVal = plannedNewVal.MarkWithPaths(unmarkedPaths)
-	}
-
 	// We allow the planned new value to disagree with configuration _values_
 	// here, since that allows the provider to do special logic like a
 	// DiffSuppressFunc, but we still require that the provider produces
@@ -303,7 +298,7 @@ func (n *EvalDiff) Eval(ctx EvalContext) (interface{}, error) {
 		return nil, diags.Err()
 	}
 
-	if errs := objchange.AssertPlanValid(schema, priorVal, configVal, plannedNewVal); len(errs) > 0 {
+	if errs := objchange.AssertPlanValid(schema, unmarkedPriorVal, unmarkedConfigVal, plannedNewVal); len(errs) > 0 {
 		if resp.LegacyTypeSystem {
 			// The shimming of the old type system in the legacy SDK is not precise
 			// enough to pass this consistency check, so we'll give it a pass here,
@@ -342,6 +337,13 @@ func (n *EvalDiff) Eval(ctx EvalContext) (interface{}, error) {
 	diags = diags.Append(ignoreChangeDiags)
 	if ignoreChangeDiags.HasErrors() {
 		return nil, diags.Err()
+	}
+
+	// Add the marks back to the planned new value -- this must happen after ignore changes
+	// have been processed
+	unmarkedPlannedNewVal := plannedNewVal
+	if len(unmarkedPaths) > 0 {
+		plannedNewVal = plannedNewVal.MarkWithPaths(unmarkedPaths)
 	}
 
 	// The provider produces a list of paths to attributes whose changes mean
@@ -406,7 +408,6 @@ func (n *EvalDiff) Eval(ctx EvalContext) (interface{}, error) {
 
 	// Unmark for this test for equality. If only sensitivity has changed,
 	// this does not require an Update or Replace
-	unmarkedPlannedNewVal, _ := plannedNewVal.UnmarkDeep()
 	eqV := unmarkedPlannedNewVal.Equals(unmarkedPriorVal)
 	eq := eqV.IsKnown() && eqV.True()
 
